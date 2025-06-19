d59b5ec45da49c3c74f0f22759df644e83d31ccd
aziyanck <aziyan916@gmail.com>
2025-06-19 21:43:55 +0530

    lazy load test 02

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@
 import ScrollDiv from './components/ScrollDiv';
 import Navbar from './components/Navbar';
 import featuresData from './components/sub_comp/features_data';
-import { lazy, Suspense } from 'react';
+import { lazy, useEffect, Suspense } from 'react';
 
 const Services = lazy(() => import('./components/Services'));
 const Features = lazy(() => import('./components/Features'));
@@ -14,12 +14,25 @@ const Review = lazy(() => import('./components/Review'));
 const Footer = lazy(() => import('./components/Footer'));
 
 function Home() {
+
+  useEffect(() => {
+    // Trigger all components to load in the background
+    import('./components/Services');
+    import('./components/Features');
+    import('./components/LogoCaro');
+    import('./components/Brands');
+    import('./components/WhyIta');
+    import('./components/Contact');
+    import('./components/Review');
+    import('./components/Footer');
+  }, []);
+
   return (
     <div className="overflow-x-clip">
       <Navbar />
       <ScrollDiv />
 
-      
+      <Suspense fallback={null}>
         <Services />
         {featuresData.map((section, index) => (
           <Features
@@ -37,7 +50,7 @@ function Home() {
         <Contact />
         <Review />
         <Footer />
-      
+      </Suspense>
     </div>
   );
 }
