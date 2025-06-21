287144db34d3d0511cecfda51280748dc4f6ac54
aziyanck <aziyan916@gmail.com>
2025-06-21 12:39:21 +0530

    admin comps defined

---
+++ src/admin_components/supabase.js
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://flflrlrtahoybbxdkkah.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZmxybHJ0YWhveWJieGRra2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0ODQ0NzYsImV4cCI6MjA2NjA2MDQ3Nn0.bhgcTKL84-mruAiKuI9si_gCX9e3C6wvtzTz3sEb9zc";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const getAll = async () => {
+  const { data, error } = await supabase.from("products").select();
+
+  if (error) {
+    console.log("error happened", error);
+    return;
+  }
+
+  if (data) {
+    return data;
+  }
+};
+
+export default getAll;
